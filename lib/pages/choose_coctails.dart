bea6e2df890c0b5021a2936ebc9a96cc06c40074
OxySqualo <oxana.xena@gmail.com>
2022-10-29 15:53:15 +0300

    33

--- lib/pages/choose_coctails.dart
+++ lib/pages/choose_coctails.dart
@@ -1,3 +1,4 @@
+import 'package:coctails_newyear/helpers/colors.dart';
 import 'package:flutter/material.dart';
 
 class Coctail extends StatefulWidget {
@@ -12,23 +13,39 @@ class _CoctailState extends State<Coctail> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Color.fromARGB(255, 218, 162, 181),
         body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/party.png"),
-                  fit: BoxFit.cover)),
+          padding: EdgeInsets.all(10),
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child: Container(
-              padding: const EdgeInsetsDirectional.only(top: 30),
-              alignment: Alignment.topCenter,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/recipe');
-                },
-                child: const Text('Смешай свой коктейль'),
-              )),
+          child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // padding: const EdgeInsetsDirectional.only(top: 30),
+              // alignment: Alignment.topCenter,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        Color.fromARGB(255, 17, 95, 20)),
+                    shadowColor: MaterialStatePropertyAll<Color>(
+                        Color.fromARGB(255, 218, 162, 181)),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/recipe');
+                  },
+                  child: const Text(
+                    'Смешай свой \nкоктейль!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(),
+                  ),
+                ),
+                Center(
+                    child: Image(
+                  image: AssetImage("assets/images/party.png"),
+                )),
+              ]),
         ),
       ),
     );
