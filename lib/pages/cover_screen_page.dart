5cc91d83bea2d7aca8362fc5ab199f20a13aa643
Melamorri <yuliapavlovich@gmail.com>
2022-10-29 13:52:55 +0300

    cover

--- lib/pages/cover_screen_page.dart
+++ lib/pages/cover_screen_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class CoverScreenPage extends StatefulWidget {
   const CoverScreenPage({super.key});
@@ -15,16 +16,15 @@ class _CoverScreenPageState extends State<CoverScreenPage> {
       // padding: const EdgeInsets.only(top: 20),
       decoration: const BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("assets/images/menubackground.png"),
-              fit: BoxFit.cover)),
+              image: AssetImage("assets/images/cover.jpg"), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
-            Image(
-                image: AssetImage('assets/images/cover.jpg'),
-                fit: BoxFit.cover),
+            // Image(
+            //     image: AssetImage('assets/images/cover.jpg'),
+            //     fit: BoxFit.cover),
           ],
         ),
       ),
