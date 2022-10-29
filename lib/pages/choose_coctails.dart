fdc73b06f24456892b08b27c2f11662b10bcc36a
OxySqualo <oxana.xena@gmail.com>
2022-10-29 15:16:52 +0300

    3

--- lib/pages/choose_coctails.dart
+++ lib/pages/choose_coctails.dart
@@ -11,26 +11,24 @@ class _CoctailState extends State<Coctail> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/cover.jpg"),
-                fit: BoxFit.cover)),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Container(
-                padding: const EdgeInsetsDirectional.only(bottom: 20),
-                alignment: Alignment.bottomCenter,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/recipe');
-                  },
-                  child: const Text('Смешай свой коктейль'),
-                )),
-          ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/party.png"),
+                  fit: BoxFit.cover)),
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Container(
+              padding: const EdgeInsetsDirectional.only(top: 30),
+              alignment: Alignment.topCenter,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/recipe');
+                },
+                child: const Text('Смешай свой коктейль'),
+              )),
         ),
       ),
     );
