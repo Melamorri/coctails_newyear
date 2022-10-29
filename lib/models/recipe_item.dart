762e7c7f222a2b3247e5aad3348ee57a5258e650
Melamorri <yuliapavlovich@gmail.com>
2022-10-29 16:32:21 +0300

    v

--- lib/models/recipe_item.dart
+++ lib/models/recipe_item.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/cupertino.dart';
 
-class Recipe{
+class Recipe {
   final int id;
   final String title;
   final String description;
   final String image;
   final List<String> ingredients;
 
-
   Recipe({
     required this.id,
     required this.title,
-    required this.description, 
-    required this.image, 
-    required this.ingredients, 
+    required this.description,
+    required this.image,
+    required this.ingredients,
   });
 }
 
-List <Recipe> recipesList = [
-  Recipe(id: 1, title: "Мандариновый глинтвейн с розмарином", description: "терпкий, согревающий и обнимающий новогодним настроением", image: "assets/images/coctails/mandarin.jpg", ingredients: ["сухое красное вино, сладкий мандарин, веточки розмарина, барный лёд, цедра лимона, палочки корицы,бутоны гвоздичного дерева или настоящий бадьян"]),
-
-
-]
+List<Recipe> recipesList = [
+  Recipe(
+      id: 1,
+      title: "Мандариновый глинтвейн с розмарином",
+      description: "терпкий, согревающий и обнимающий новогодним настроением",
+      image: "assets/images/coctails/mandarin.jpg",
+      ingredients: [
+        "сухое красное вино - 150 мл, сладкий мандарин - 60 г, веточки розмарина 5г, барный лёд, цедра лимона, палочки корицы - 5г,гвоздика - 3 г"
+      ]),
+  Recipe(
+      id: 2,
+      title: "Red square",
+      description:
+          'если бы Джеймс Бонд отмечал Новый год в России, он бы выбрал этот коктейль',
+      image: "assets/images/coctails/red-square.jpg",
+      ingredients: [
+        "Бехеровка - 50 мл, Сироп Ваниль - 10 мл, Лимон1/2 штуки, Малина10 шт, Розмарин, Лед в кубиках"
+      ]),
+  Recipe(
+      id: 3,
+      title: "Снежная королева",
+      description: "коктейль, которым больше любуешься, чем наслаждаешься",
+      image: "assets/images/coctails/ice_queen,jpeg",
+      ingredients: [
+        "итальянское проссеко, сироп блю-кюросао, кандурин, веточка свежей мяты, нежная, мятная пенка"
+      ])
+];
