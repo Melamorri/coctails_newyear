3070687c2e8c907fbd1b69637703837a0171db83
Melamorri <yuliapavlovich@gmail.com>
2022-10-29 13:48:47 +0300

    cover main

--- lib/pages/cover_screen_page.dart
+++ lib/pages/cover_screen_page.dart
@@ -1,1 +1,33 @@
 import 'package:flutter/material.dart';
+
+class CoverScreenPage extends StatefulWidget {
+  const CoverScreenPage({super.key});
+
+  @override
+  State<CoverScreenPage> createState() => _CoverScreenPageState();
+}
+
+class _CoverScreenPageState extends State<CoverScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+      // padding: const EdgeInsets.only(top: 20),
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/menubackground.png"),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(
+                image: AssetImage('assets/images/cover.jpg'),
+                fit: BoxFit.cover),
+          ],
+        ),
+      ),
+    ));
+  }
+}
